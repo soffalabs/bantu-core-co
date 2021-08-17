cb9d5fd1e7eb32054bcd90a4170e9cce156e98f2
Titus GAHISSY <titus@gahissy.com>
2021-08-17 22:43:55 +0100

    Refactoring

--- commons.go
+++ commons.go
@@ -6,7 +6,6 @@ const (
 
 
 type ServiceConfig struct {
-	Port               int    `json:"server.port" env:"PORT" envDefault:"8080"`
 	DatabaseUrl        string `json:"db.url" env:"DATABASE_URL"`
 	TenantsDatabaseUrl string `json:"db.tenants_url" env:"TENANTS_DATABASE_URL"`
 	AmqpUrl            string `json:"amqp.url" env:"AMQP_URL"`
